1cdb87f7f5eab21e98b84270ec9c8db0d2d07400
tomas-nekut <58033664+tomas-nekut@users.noreply.github.com>
2022-11-04 15:49:46 +0900

    refactor

--- server/face_landmarks_list.py
+++ server/face_landmarks_list.py
@@ -3,20 +3,32 @@
 
 # Oru Adaar Love, Manikya Malaraya Poovi Song Video
 # expresions https://www.youtube.com/watch?v=embYkODkzcs MDI Management Development International
+# cv2 gpu https://forum.opencv.org/t/can-i-use-opencv-python-with-gpu/8947/2
 
-from math import degrees
-from re import T
 import numpy as np
-#from skimage import transform as tf
-import mediapipe as mp
-#from rigid_transform_3D import rigid_transform_3D
-#from sklearn.metrics import mean_squared_error
-from scipy.spatial.transform import Rotation as R
 import cv2
+import mediapipe as mp
+from scipy.spatial.transform import Rotation as R
+from forbiddenfruit import curse
+
+curse(np.ndarray, 'to_XY', lambda x: x[:,0:2])
 
 class FaceLandmarksDetector(mp.solutions.face_mesh.FaceMesh):
-    def __init__(self, video_path=None, smoothing_cnt=5):
+    def __init__(self):
         super().__init__(static_image_mode=True, max_num_faces=1, refine_landmarks=True)
+    
+    def __process_numpy(self, img):
+        landmarks = super().process(img).multi_face_landmarks[0].landmark
+        landmarks = np.array([(l.x*img.shape[1], l.y*img.shape[0], l.z*img.shape[1]) for l in landmarks])
+        return landmarks
+    
+    def process(self, img):
+        landmarks = self.__process_numpy(img)
+        return FaceLandmarksList(landmarks)
+
+class FaceLandmarksVideoDetector(FaceLandmarksDetector):
+    def __init__(self, video_path=None, smoothing_cnt=5):
+        super().__init__()
         if video_path != None:
             self.open(video_path)
         self.__smoothing_cnt = smoothing_cnt
@@ -24,6 +36,15 @@ class FaceLandmarksDetector(mp.solutions.face_mesh.FaceMesh):
     def open(self, video_path):
         self.__cap = cv2.VideoCapture(video_path)
     
+    def get_frame_width(self):
+        return int(self.__cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+
+    def get_frame_height(self):
+        return int(self.__cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    def get_frame_size(self):
+        return self.get_frame_width(), self.get_frame_height()
+
     def __iter__(self):
         self.__cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
         self.__landmarks = None
@@ -55,25 +76,6 @@ class FaceLandmarksDetector(mp.solutions.face_mesh.FaceMesh):
             if self.__landmarks.shape[0] > self.__smoothing_cnt:
                 self.__landmarks = self.__landmarks[1:]
             
-    def __process_numpy(self, img):
-        landmarks = super().process(img).multi_face_landmarks[0].landmark
-        landmarks = np.array([(l.x*img.shape[1], l.y*img.shape[0], l.z*img.shape[1]) for l in landmarks])
-        return landmarks
-    
-    def process(self, img):
-        landmarks = self.__process_numpy(img)
-        return FaceLandmarksList(landmarks)
-    
-    def get_frame_width(self):
-        return int(self.__cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-
-    def get_frame_height(self):
-        return int(self.__cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-    def get_frame_size(self):
-        return self.get_frame_width(), self.get_frame_height()
-
-    
 class FaceLandmarksList():
     def __init__(self, landmarks):
         self.__landmarks = landmarks[:468]  # only mesh
@@ -81,57 +83,52 @@ class FaceLandmarksList():
 
     def __getitem__(self, index):
         return self.__landmarks[index]
-    
+
     def copy(self):
         return FaceLandmarksList(self.__landmarks)
 
+    def get_mesh(self):
+        return self.__landmarks
+
     def get_left_eye_outline(self):
-        return self[[33,246,161,160,159,158,157,173,133,155,154,153,145,144,163,7],0:2].astype(np.int32)
+        return self.__landmarks[[33,246,161,160,159,158,157,173,133,155,154,153,145,144,163,7]]
 
     def get_right_eye_outline(self):
-        return self[[362,398,384,385,386,387,388,466,263,249,390,373,374,380,381,382],0:2].astype(np.int32)
+        return self.__landmarks[[362,398,384,385,386,387,388,466,263,249,390,373,374,380,381,382]]
 
     def get_mouth_outline(self):
-        return self[[78,191,80,81,82,13,312,311,310,415,308,324,318,402,317,14,87,178,88,95],0:2].astype(np.int32)
-
-    def get_mouth_line(self):
-        return self[[57,61,78,191,80,81,82,13,312,311,310,415,308,291,287],0:2].astype(np.int32)
-        
+        return self.__landmarks[[78,191,80,81,82,13,312,311,310,415,308,324,318,402,317,14,87,178,88,95]]
+   
     def get_teeth_line(self, type='middle'):
         teeth_line_left = [[-0.23, 0.398, 0.45], [-0.22, 0.396, 0.32], [-0.18, 0.394, 0.18], [-0.1, 0.392, 0.1], [0., 0.39, 0.05]]
         teeth_line_right = [(-x,y,z) for (x,y,z) in teeth_line_left[3::-1]]
         teeth_line = teeth_line_left + teeth_line_right
         teeth_line = np.array(teeth_line)
-        
         if type=='upper':
             teeth_line = teeth_line + np.array([0,-0.06,0])
         elif type=='lower':
             teeth_line = teeth_line + np.array([0, 0.06,0])
-
         return self.__denormalize(teeth_line)
-        #return teeth_line
-        #  X -->   Y \/   Z -->
 
     def translate(self, vectors, exclude_boudary=True):
+        # prevent boundary landmarks from moving by clearing their translation vectors
         if exclude_boudary:
             vectors[[127,162,21,54,103,67,109,10,338,297,332,284,251,389,368,264,356,447,454,366,323,401,361,435,288,367,397,364,365,394,379,378,400,377,152,148,176,149,150,169,136,135,172,138,58,215,177,132,137,93,227,234,34]] = 0
-        landmarks = self.get_normalized()
+        # apply translation to normalized landmarks
+        landmarks = self.__get_normalized()
         landmarks += vectors
-
+        # move certain landmarks that tends to overlap and cause interpolation problem 
         for u,l in [(398,381),(384,380),(385,374),(386,373),(387,390),(388,249)]:
             if landmarks[u][1] + 0.01 > landmarks[l][1]:
                 landmarks[u][1] = landmarks[l][1] - 0.01
-
         for u,l in [(17,18),(84,83),(181,182),(314,313),(405,406)]:
             if landmarks[u][1] + 0.02 > landmarks[l][1]:
                 landmarks[l][1] = landmarks[u][1] + 0.02
-                
-        landmarks -= self.__norm_translate
-        landmarks /= self.__norm_scale
-        landmarks = self.__norm_rotation.apply(landmarks, inverse=True)
+        # denormalize
+        landmarks = self.__denormalize(landmarks)
         return FaceLandmarksList(landmarks)
 
-    def get_normalized(self):
+    def __compute_normalization(self):
         if self.__normalized_landmarks is None:
             # rotation
             vec1 = self[447]-self[227]
@@ -149,60 +146,19 @@ class FaceLandmarksList():
             self.__norm_scale = 1/(self.__normalized_landmarks.max(axis=0)-self.__normalized_landmarks.min(axis=0))
             self.__normalized_landmarks *= self.__norm_scale
             # translation
-            self.__norm_translate = -self.__normalized_landmarks[6] #4
+            self.__norm_translate = -self.__normalized_landmarks[6] 
             self.__normalized_landmarks += self.__norm_translate
-        return self.__normalized_landmarks.copy()
+        return self.__normalized_landmarks.copy(), (self.__norm_translate, self.__norm_scale, self.__norm_rotation)
         
+    def __get_normalized(self):
+        normalized_landmarks, _ = self.__compute_normalization()
+        return normalized_landmarks
+
     def __denormalize(self, points):
-        _ = self.get_normalized()
+        _, (norm_translate, norm_scale, norm_rotation) = self.__compute_normalization()
         points = points.copy()
-        points -= self.__norm_translate
-        points /= self.__norm_scale
-        points = self.__norm_rotation.apply(points, inverse=True)
+        points -= norm_translate
+        points /= norm_scale
+        points = norm_rotation.apply(points, inverse=True)
         return points
         
-    def __angle(self, a, b):
-        return np.arccos(np.dot(a,b) / np.abs(np.linalg.norm(a) * np.linalg.norm(b)))
-    
-    def to_numpy(self):
-        return np.array(list(self))
-
-    #def from_numpy(self, landmarks):
-    #    self.__landmarks = landmarks
-    '''
-    def get_points_generator(self, landmark_indexes):
-        for i in landmark_indexes:
-            yield self[i]
-    def get_points(self, landmark_indexes):
-        return np.array(list(self.get_points_generator(landmark_indexes)))
-    def get_vectors(self, landmark_vector_indexes):
-        # landmark_vector_indexes is [(landmark_indexe1, landmark_indexe2, norm_length), ...] 
-        # where 
-        vects = [ self[l1] + self[l2] for l1,l2 in landmark_vector_indexes ]
-        vects = np.array(vects)
-        a = np.zeros(vects.shape, dtype=np.int32)
-        a[:,0] = vects[:,0]
-        a[:,1] = vects[:,1]
-        a[:,2] = vects[:,2] - vects[:,0]
-        a[:,3] = vects[:,3] - vects[:,1]
-        return a
-    '''
-
-    
-
-
-    def __normalize(self):
-        src = self.get_points([197,4])
-        dst = np.array([[500,0],[500,200]])
-        tform = tf.estimate_transform('similarity', src, dst)
-       
-        print(src)
-        print(dst) 
-        print(tform.params)
-
-        mt = tf.matrix_transform(self.to_numpy(), tform.params)#mt is the same dst
-        #mean_squared_error(mt,dst) #should be zero
-        #print( '{:.10f}'.format(mean_squared_error(mt,dst)) )
-        return mt
-
-
--- server/script.py
+++ server/script.py
@@ -1,118 +1,80 @@
-from itertools import tee
-from pydoc import plain
-import sys
 import cv2
 import imageio
 from scipy import interpolate
 import numpy as np
-from flask import Flask, Response, request
-import requests
-import io
-from PIL import Image
-from io import BytesIO
-import matplotlib.pyplot as plt
 from face_landmarks_list import FaceLandmarksDetector
 from tqdm import tqdm
 
-img = cv2.imread("../test/zeman3.jpg")
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-landmarks = FaceLandmarksDetector().process(img)
-
-
-def add_teeth(original_img):
-    img = original_img.copy()
-    teeth = cv2.imread("teeth.jpg")
-    teeth = cv2.cvtColor(teeth, cv2.COLOR_BGR2RGB)
-
-    bg = img
-
-    t = np.zeros_like(img)
-    t[0:teeth.shape[0], 0:teeth.shape[1]] = teeth
-
-
-    h = FaceLandmarksDetector().process(bg).get_teeth_line()
-    # find local max
-    min = h[:,0].argmin()
-    max = h[:,0].argmax()
-    teeth_line_upper  = landmarks.get_teeth_line('upper') [min:max+1]
-    teeth_line_middle = landmarks.get_teeth_line('middle')[min:max+1]
-    teeth_line_lower  = landmarks.get_teeth_line('lower') [min:max+1]
-    teeth_line = np.vstack([teeth_line_upper,teeth_line_middle,teeth_line_lower])
-    h = teeth_line[:,:2]
-
-    root = lambda x: np.sign(x) * (abs(x)**(0.7))
-    c = np.linspace(0,1,9)  # len landmarks.get_teeth_line()
-    c = ((root(2*c-1)))*(1/2)+(1/2)
-    print(c)
-    c *= teeth.shape[1]
-    c = c[min:(max+1)]
-    print(c)
-    print("teeth.shape[1]", teeth.shape[1])
-    g = np.expand_dims(c, axis=1)
-
-    g = np.hstack([g, np.ones_like(g)*teeth.shape[0]/2]).astype(np.int32)
-    a = g.copy()
-    a[:,1] = 0
-    b = g.copy()
-    b[:,1] = teeth.shape[0]
-    g = np.vstack([a,g,b])
-
-    print(h.shape)
-    print(g.shape)
-
-
-
-    #for v,b in zip(h,g):
-    #    cv2.line(bg, v.astype(np.int32), b.astype(np.int32), (255,255,255), 1)
-        
-
-    f_x = interpolate.LinearNDInterpolator(h, g[:,0])
-    f_y = interpolate.LinearNDInterpolator(h, g[:,1])
-    x = f_x(*np.meshgrid(np.arange(t.shape[1]), np.arange(t.shape[0]))).astype(np.float32)
-    y = f_y(*np.meshgrid(np.arange(t.shape[1]), np.arange(t.shape[0]))).astype(np.float32)
-    remaped_t = cv2.remap(t, x, y, interpolation=cv2.INTER_LINEAR, borderValue=0, borderMode=cv2.BORDER_CONSTANT) 
-
-    bg = np.where(remaped_t != 0, remaped_t, bg)
-    return bg
-
-'''
-for (x,y) in h:    
-    cv2.circle(remaped_t, (int(x),int(y)), 1, (255, 255, 255), -1)
-
-for (x,y) in g:    
-    cv2.circle(remaped_t, (int(x),int(y)), 1, (255, 255, 255), -1)
-'''
-
-#cv2.imwrite("../test/bg.jpg", bg)
-#exit()
-
-bg = add_teeth(img)
-
-# fill eyes and mouth with black
-poly = [landmarks.get_left_eye_outline(), landmarks.get_right_eye_outline(), landmarks.get_mouth_outline()]
-cv2.fillPoly(img, pts=poly, color=(0, 0, 0))  
-
-result_list = []
-frame_rate = 8
-
-for vectors in tqdm(np.load("motion_vectors.npy")[::int(24/frame_rate)]):
-
-    translated_landmarks = landmarks.translate(vectors)
-   
-    # interpolate 
-    f_x = interpolate.LinearNDInterpolator(translated_landmarks.to_numpy()[:,0:2], landmarks.to_numpy()[:,0])
-    f_y = interpolate.LinearNDInterpolator(translated_landmarks.to_numpy()[:,0:2], landmarks.to_numpy()[:,1])
-    
-    # evaluete interpolation function for each pixel of input image
+# mapping from input to target image is described by set of source point -> destination point pairs 
+def remap_image(img, src_points, dst_points):
+    # interpolate source point for each pixel of the image
+    f_x = interpolate.LinearNDInterpolator(dst_points, src_points[:,0])
+    f_y = interpolate.LinearNDInterpolator(dst_points, src_points[:,1])
     x = f_x(*np.meshgrid(np.arange(img.shape[1]), np.arange(img.shape[0]))).astype(np.float32)
     y = f_y(*np.meshgrid(np.arange(img.shape[1]), np.arange(img.shape[0]))).astype(np.float32)
-   
+    # transform image
     remaped_img = cv2.remap(img, x, y, interpolation=cv2.INTER_LINEAR, borderValue=0, borderMode=cv2.BORDER_CONSTANT) 
+    return remaped_img
 
-    poly = [translated_landmarks.get_left_eye_outline(), translated_landmarks.get_right_eye_outline(), translated_landmarks.get_mouth_outline()]
-    cv2.fillPoly(remaped_img, pts=poly, color=(0, 0, 0))  
-   
-    result = np.where(remaped_img != 0, remaped_img, bg)
-    result_list.append(result)
-       
-imageio.mimsave('../test/out.gif', result_list, fps=frame_rate)
+# 
+def add_teeth(original_img):
+    teeth = cv2.imread("teeth.jpg")
+    teeth = cv2.cvtColor(teeth, cv2.COLOR_BGR2RGB)
+    teeth = cv2.resize(teeth, dsize=(original_img.shape[1],original_img.shape[0]))
+    landmarks = FaceLandmarksDetector().process(original_img)
+    # transform teeth image to match mouth position of face in original image
+    # find part of teeth line that is visible given view angle
+    teeth_line_start = landmarks.get_teeth_line()[:,0].argmin()
+    teeth_line_end = landmarks.get_teeth_line()[:,0].argmax() + 1 
+    # find source points of transformation 
+    # src points are nelineary positioned to compensate for frontal teeth image (not panoramatic)
+    src_points = np.linspace(0, 1, len(landmarks.get_teeth_line()))  
+    root = lambda x: np.sign(x) * (abs(x)**(0.7))
+    src_points = ((root(2*src_points-1)))*(1/2)+(1/2)
+    src_points *= teeth.shape[1]
+    src_points = src_points.astype(np.int32)
+    src_points = src_points[teeth_line_start:teeth_line_end]
+    src_points = np.expand_dims(src_points, axis=1)
+    src_points = np.hstack([src_points, np.zeros_like(src_points)])
+    # upper, middle, lower teeth line
+    upper_src_points, middle_src_points, lower_src_points = src_points.copy(),src_points.copy(),src_points.copy()
+    upper_src_points[:,1] = 0
+    middle_src_points[:,1] = int(teeth.shape[0]/2)
+    lower_src_points[:,1] = teeth.shape[0]
+    src_points = np.vstack([upper_src_points,middle_src_points,lower_src_points])
+    # find destination points of transformation
+    upper_dst_points = landmarks.get_teeth_line('upper')[teeth_line_start:teeth_line_end].to_XY()
+    middle_dst_points = landmarks.get_teeth_line('middle')[teeth_line_start:teeth_line_end].to_XY()
+    lower_dst_points = landmarks.get_teeth_line('lower')[teeth_line_start:teeth_line_end].to_XY()
+    dst_points = np.vstack([upper_dst_points,middle_dst_points,lower_dst_points])
+    # transform teeth img using source points -> destination points mapping
+    remaped_teeth = remap_image(teeth, src_points, dst_points) 
+    # outside mapping region fill with original image 
+    target_image = np.where(remaped_teeth != 0, remaped_teeth, original_img)
+    return target_image
+
+def fill_eye_mouth_with_black(img, landmarks):
+    poly = [landmarks.get_left_eye_outline().to_XY().astype(int), 
+            landmarks.get_right_eye_outline().to_XY().astype(int), 
+            landmarks.get_mouth_outline().to_XY().astype(int)]
+    img = cv2.fillPoly(img, pts=poly, color=(0, 0, 0))  
+    return img
+
+def alter_image(img):
+    landmarks = FaceLandmarksDetector().process(img)
+    background = add_teeth(img)
+    img = fill_eye_mouth_with_black(img, landmarks)
+    result_list = []
+    frame_rate = 8
+    for vectors in tqdm(np.load("motion_vectors.npy")[::int(24/frame_rate)]):
+        translated_landmarks = landmarks.translate(vectors)
+        remaped_img = remap_image(img, landmarks.get_mesh().to_XY(), translated_landmarks.get_mesh().to_XY())
+        remaped_img = fill_eye_mouth_with_black(remaped_img, translated_landmarks)  
+        result = np.where(remaped_img != 0, remaped_img, background)
+        result_list.append(result)   
+    imageio.mimsave('../test/out.gif', result_list, fps=frame_rate)
+
+
+img = cv2.imread("../test/zeman3.jpg")
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+alter_image(img)
